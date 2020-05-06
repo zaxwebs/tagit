0e17c133144c0b11595d4a114401fe030c919917
zaxwebs <zifidigital@gmail.com>
2020-05-07 03:44:36 +0530

    Added Writing category

--- src/data.js
+++ src/data.js
@@ -1,61 +1,66 @@
 export const initialNetworks = {
-	facebook: {
-		name: 'Facebook',
-		characters: 63206,
-		selected: true
-	},
-	instagram: {
-		name: 'Instagram',
-		characters: 2200,
-		tags: 30,
-		selected: true
-	},
-	twitter: {
-		name: 'Twitter',
-		characters: 280,
-		selected: true
-	}
+  facebook: {
+    name: 'Facebook',
+    characters: 63206,
+    selected: true,
+  },
+  instagram: {
+    name: 'Instagram',
+    characters: 2200,
+    tags: 30,
+    selected: true,
+  },
+  twitter: {
+    name: 'Twitter',
+    characters: 280,
+    selected: true,
+  },
 }
 
 export const categories = {
-	webDesign: {
-		name: 'Web Design',
-		tags:
-			'#webdesign #ui #ux #coding #design #javascript #css #developer #entrepreneur #programmer #appdesign #art #code #tech #userexperience #startup #photography #advertising #illustrator #photoshop #dribbble #userinterface #websitedesigner #digital #webdev #brand #websitedevelopment #contentmarketing #designinspiration #websites'
-	},
-	programming: {
-		name: 'Programming',
-		tags:
-			'#programming #coding #programmer #developer #code #coder #technology #python #tech #javascript #webdeveloper #java #html #computerscience #css #webdesign #webdevelopment #software #softwaredeveloper #computer #codinglife #programminglife #hacker #linux #php #hacking #js #web #webdev'
-	},
-	graphicDesign: {
-		name: 'Graphic Design',
-		tags:
-			'#graphicdesign #design #art #logo #illustration #branding #graphicdesigner #graphic #designer #creative #artist #photoshop #digitalart #illustrator #typography #marketing #artwork #photography #graphics #drawing #uidesign #logodesign #webdesign #adobe #logodesigner #d #digitalmarketing #poster #posterdesign'
-	},
-	food: {
-		name: 'Food',
-		tags:
-			'#hungry #foodgasm #instafood #instafood #yum #yummy #yumyum #delicious #eat #dinner #food #foodporn #stuffed #hot #beautiful #breakfast #lunch #love #sharefood #homemade #sweet #delicious #eating #foodpic #foodpics #amazing #instagood #photooftheday #fresh'
-	},
-	art: {
-		name: 'Art',
-		tags:
-			'#art #artist #artistic #artists #arte #myart #artwork #illustration #graphicdesign #graphic #color #colour #colorful #painting #drawing #drawings #creative #beautiful'
-	},
-	music: {
-		name: 'Music',
-		tags:
-			'#music #love #hiphop #rap #singer #art #dj #musician #artist #musica #rock #dance #like #guitar #party #follow #livemusic #instagood #song #concert #live #instagram #photography #producer #newmusic #instamusic #s #band #video #rapper #trap #musicvideo #life #pop #beats #fun #metal #youtube #musically #m  #spotify #festival #photooftheday #musicians #soundcloud #cover #bass #friends #musicproducer #songs #songwriter #happy #rnb #radio #piano #beautiful #guitarist'
-	},
-	restaurant: {
-		name: 'Restaurant',
-		tags:
-			'#restaurant #food #foodporn #foodie #instafood #dinner #bar #lunch #delicious #chef #yummy #instagood #cafe #foodphotography #foodlover #hotel #foodstagram #wine #pizza #foodblogger #love #restaurante #eat #drinks #travel #like #cocktails #tasty #foodies #cuisine'
-	},
-	hotel: {
-		name: 'Hotel',
-		tags:
-			'#hotel #travel #restaurant #luxury #hotels #holiday #resort #vacation #love #bar #design #spa #food #instagood #summer #architecture #like #photography #indonesia #interiordesign #tourism #travelgram #instagram #relax #cafe #hotelroom #lifestyle #turismo'
-	}
+  webDesign: {
+    name: 'Web Design',
+    tags:
+      '#webdesign #ui #ux #coding #design #javascript #css #developer #entrepreneur #programmer #appdesign #art #code #tech #userexperience #startup #photography #advertising #illustrator #photoshop #dribbble #userinterface #websitedesigner #digital #webdev #brand #websitedevelopment #contentmarketing #designinspiration #websites',
+  },
+  programming: {
+    name: 'Programming',
+    tags:
+      '#programming #coding #programmer #developer #code #coder #technology #python #tech #javascript #webdeveloper #java #html #computerscience #css #webdesign #webdevelopment #software #softwaredeveloper #computer #codinglife #programminglife #hacker #linux #php #hacking #js #web #webdev',
+  },
+  graphicDesign: {
+    name: 'Graphic Design',
+    tags:
+      '#graphicdesign #design #art #logo #illustration #branding #graphicdesigner #graphic #designer #creative #artist #photoshop #digitalart #illustrator #typography #marketing #artwork #photography #graphics #drawing #uidesign #logodesign #webdesign #adobe #logodesigner #d #digitalmarketing #poster #posterdesign',
+  },
+  food: {
+    name: 'Food',
+    tags:
+      '#hungry #foodgasm #instafood #instafood #yum #yummy #yumyum #delicious #eat #dinner #food #foodporn #stuffed #hot #beautiful #breakfast #lunch #love #sharefood #homemade #sweet #delicious #eating #foodpic #foodpics #amazing #instagood #photooftheday #fresh',
+  },
+  art: {
+    name: 'Art',
+    tags:
+      '#art #artist #artistic #artists #arte #myart #artwork #illustration #graphicdesign #graphic #color #colour #colorful #painting #drawing #drawings #creative #beautiful',
+  },
+  music: {
+    name: 'Music',
+    tags:
+      '#music #love #hiphop #rap #singer #art #dj #musician #artist #musica #rock #dance #like #guitar #party #follow #livemusic #instagood #song #concert #live #instagram #photography #producer #newmusic #instamusic #s #band #video #rapper #trap #musicvideo #life #pop #beats #fun #metal #youtube #musically #m  #spotify #festival #photooftheday #musicians #soundcloud #cover #bass #friends #musicproducer #songs #songwriter #happy #rnb #radio #piano #beautiful #guitarist',
+  },
+  restaurant: {
+    name: 'Restaurant',
+    tags:
+      '#restaurant #food #foodporn #foodie #instafood #dinner #bar #lunch #delicious #chef #yummy #instagood #cafe #foodphotography #foodlover #hotel #foodstagram #wine #pizza #foodblogger #love #restaurante #eat #drinks #travel #like #cocktails #tasty #foodies #cuisine',
+  },
+  hotel: {
+    name: 'Hotel',
+    tags:
+      '#hotel #travel #restaurant #luxury #hotels #holiday #resort #vacation #love #bar #design #spa #food #instagood #summer #architecture #like #photography #indonesia #interiordesign #tourism #travelgram #instagram #relax #cafe #hotelroom #lifestyle #turismo',
+  },
+  webDesign: {
+    name: 'Writing',
+    tags:
+      '#creativewriting #instagood #book #authors #poetry #poets #follow #writingprompts #story #writinginspiration #like #writings #literature #creative #writersnetwork #writingtips #novel #writingcommunity #writerforhire',
+  },
 }
